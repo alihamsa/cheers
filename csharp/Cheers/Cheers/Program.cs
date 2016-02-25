9d765ab5e49cc44d262f9e6237366dad23fbe65b
alihamsa <sharif4203@hotmail.com>
2016-02-24 20:33:24 -0600

    part one of cheers

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -10,6 +10,35 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("what is your name");
+            var name = Console.ReadLine();
+
+            char[] NewName = name.ToCharArray();
+            char[] consonant = "halfnorsemix".ToCharArray();
+
+            foreach (char TempVar in NewName)
+            {
+                if (consonant.Contains(TempVar))
+                {
+                    Console.WriteLine("give me an " + TempVar);
+                }
+                else
+                {
+                    Console.WriteLine("give me a " + TempVar);
+                }
+
+                //Console.WriteLine(TempVar + " is grand");
+            }
+            Console.WriteLine(name + " is grand");
+
+
+
+
+
+
+
+
+
         }
     }
 }
